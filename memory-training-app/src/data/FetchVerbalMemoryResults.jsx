b8c5e8ded5a024d15d6f78181d222037200561e4
Julia Szczepanek <jszczepanek@griddynamics.com>
2025-02-17 12:53:44 +0100

    feat: adding data fetching components

---
+++ memory-training-app/src/data/FetchVerbalMemoryResults.jsx
@@ -0,0 +1,25 @@
+import { collection, getDocs, doc } from "firebase/firestore";
+import { db } from "../firebase";
+
+export const FetchVerbalMemoryResults = async (currentUser) => {
+  if (!currentUser) {
+    throw new Error("User is not logged in!");
+  }
+
+  try {
+    const userDocRef = doc(db, "userResults", currentUser.uid);
+    const resultsCollectionRef = collection(userDocRef, "verbalMemoryResults");
+
+    const querySnapshot = await getDocs(resultsCollectionRef);
+
+    const results = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    return results;
+  } catch (error) {
+    console.error("Błąd podczas odczytu danych z Firestore:", error);
+    return [];
+  }
+};
